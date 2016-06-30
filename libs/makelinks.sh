56986366af42d30ebf9c473693c2974a5eefaf9f
Eric Allman <eric@cs.berkeley.edu>
2016-06-30 13:47:55 -0700

    Apparently Jenkins uses the -e (errexit) flag for all shells, which causes makelinks.sh to fail.

--- libs/makelinks.sh
+++ libs/makelinks.sh
@@ -4,26 +4,26 @@ lib=$1
 major=$2
 minor=$3
 
-rm lib$lib.so.$major.$minor
+rm lib$lib.so.$major.$minor || true
 cp ../$lib/lib$lib.so.$major.$minor .
 . ../adm/common-support.sh
 
 platform OS
 case "$OS" in
     "ubuntu" | "debian" | "freebsd" | "centos")
-    	rm lib$lib.so.$major lib$lib.so
+    	rm lib$lib.so.$major lib$lib.so || true
 	ln -s lib$lib.so.$major.$minor lib$lib.so.$major
 	ln -s lib$lib.so.$major lib$lib.so
 	;;
 
 "darwin")
-	rm lib$lib.dylib lib$lib.$major.$minor.dylib
+	rm lib$lib.dylib lib$lib.$major.$minor.dylib || true
 	mv lib$lib.so.$major.$minor lib$lib.$major.$minor.dylib
 	ln -s lib$lib.$major.$minor.dylib lib$lib.dylib
 	;;
 
 "redhat")
-	rm lib$lib-$major.$minor.so lib$lib.so.$major
+	rm lib$lib-$major.$minor.so lib$lib.so.$major || true
 	mv lib$lib.so.$major.$minor lib$lib-$major.$minor.so
 	ln -s lib$lib-$major.$minor.so lib$lib.so.$major
 	;;
