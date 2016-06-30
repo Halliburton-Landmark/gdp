851390780eb3624b6f5b417a8e14d5fc9e424ad7
Eric Allman <eric@cs.berkeley.edu>
2016-06-30 15:51:05 -0700

    Fix NULL dereference in gdplogd.

--- gdplogd/logd_disklog.c
+++ gdplogd/logd_disklog.c
@@ -1156,7 +1156,8 @@ disk_read(gdp_gcl_t *gcl,
 
 	EP_ASSERT_POINTER_VALID(gcl);
 	gdp_buf_reset(datum->dbuf);
-	gdp_buf_reset(datum->sig);
+	if (datum->sig != NULL)
+		gdp_buf_reset(datum->sig);
 
 	ep_dbg_cprintf(Dbg, 14, "disk_read(%" PRIgdp_recno "): ", datum->recno);
 
