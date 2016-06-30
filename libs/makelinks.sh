5354ded8da999c8fd53655f6d735bbe1ad17df0d
Eric Allman <eric@cs.berkeley.edu>
2016-06-30 11:13:43 -0700

    Change handling of shared libraries so links are created dynamically in libs directory rather than being stored (with version numbers) in git.

---
+++ libs/makelinks.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+lib=$1
+major=$2
+minor=$3
+
+rm lib$lib.so.$major.$minor
+cp ../$lib/lib$lib.so.$major.$minor .
+. ../adm/common-support.sh
+
+platform OS
+case "$OS" in
+    "ubuntu" | "debian" | "freebsd" | "centos")
+    	rm lib$lib.so.$major lib$lib.so
+	ln -s lib$lib.so.$major.$minor lib$lib.so.$major
+	ln -s lib$lib.so.$major lib$lib.so
+	;;
+
+"darwin")
+	rm lib$lib.dylib lib$lib.$major.$minor.dylib
+	mv lib$lib.so.$major.$minor lib$lib.$major.$minor.dylib
+	ln -s lib$lib.$major.$minor.dylib lib$lib.dylib
+	;;
+
+"redhat")
+	rm lib$lib-$major.$minor.so lib$lib.so.$major
+	mv lib$lib.so.$major.$minor lib$lib-$major.$minor.so
+	ln -s lib$lib-$major.$minor.so lib$lib.so.$major
+	;;
+esac
