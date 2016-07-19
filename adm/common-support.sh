3756d4acad87cba7fb5960863cc6130319b181e2
Eric Allman <eric@cs.berkeley.edu>
2016-07-19 13:00:04 -0700

    Code consolidation; create empty log file on installation; install systemd configuration in init-gdp-server.sh.

--- adm/common-support.sh
+++ adm/common-support.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+{ test -r /usr/local/etc/gdp.conf.sh && . /usr/local/etc/gdp.conf.sh; } ||
+	{ test -r /etc/gdp.conf.sh && . /etc/gdp.conf.sh; }
 
 #
 #  These macros are intended to be sourced into other shell files
@@ -16,6 +18,10 @@ Pur='[0;35m';     BPur='[1;35m';    UPur='[4;35m';    IPur='[0;95m';    BIPu
 Cya='[0;36m';     BCya='[1;36m';    UCya='[4;36m';    ICya='[0;96m';    BICya='[1;96m';   On_Cya='[46m';    On_ICya='[0;106m';
 Whi='[0;37m';     BWhi='[1;37m';    UWhi='[4;37m';    IWhi='[0;97m';    BIWhi='[1;97m';   On_Whi='[47m';    On_IWhi='[0;107m';
 
+#################### FUNCTIONS ####################
+
+
+# Error/Information messages
 info() {
 	echo "${Gre}${On_Bla}[INFO] $1${Reset}"
 }
@@ -28,6 +34,33 @@ fatal() {
 	echo "${Whi}${On_Red}[FATAL] $1${Reset}"
 	exit 1
 }
+
+# Create a directory as the user gdp
+mkdir_gdp() {
+	test -d $1 && return
+	info "Creating $1 as ${GDP_USER}:${GDP_GROUP}"
+	sudo mkdir -p $1
+	sudo chmod ${2:-0755} $1
+	sudo chown ${GDP_USER}:${GDP_GROUP} $1
+}
+
+#################### END OF FUNCTIONS ####################
+
+# configure defaults
+: ${GDP_ROOT:=/usr}
+if [ "$GDP_ROOT" = "/usr" ]
+then
+	: ${GDP_ETC:=/etc/gdp}
+	: ${EP_PARAMS:=/etc/ep_adm_params}
+else
+	: ${GDP_ETC:=$GDP_ROOT/etc}
+fi
+: ${GDP_USER:=gdp}
+: ${GDP_GROUP:=$GDP_USER}
+: ${GDP_LOG_DIR:=/var/log/gdp}
+: ${GDP_VAR:=/var/swarm/gdp}
+: ${GDP_KEYS_DIR:=$GDP_ETC/keys}
+: ${GDPLOGD_DATADIR:=$GDP_VAR/gcls}
 
 OS=""
 OSVER=""
