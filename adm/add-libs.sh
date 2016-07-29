51113c368f796170d84c8aff33d5d095bf829fbe
Eric Allman <eric@cs.berkeley.edu>
2016-07-28 21:26:55 -0700

    Arrange to dynamically choose libraries based on environment and conditions.  This isn't completely satisfying.

---
+++ adm/add-libs.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#
+#  This script outputs additional flags for use when loading binaries.
+#  For example, it might output "-ldb" if this system has the Berkeley
+#  db library in a separate library (as opposed to being part of libc).
+#
+#  The only parameter is the name of the module being loaded.  In
+#  particular, gdplogd needs libraries that normal apps do not.
+#
+
+module=${1-none}
+
+if [ "$module" = "gdplogd" ]
+then
+	candidates=`ls \
+		/lib/libdb.* \
+		/usr/lib/libdb.* \
+		/usr/lib/*/libdb.* \
+		/usr/local/lib/libdb.* \
+		2>/dev/null`
+	if [ ! -z "$candidates" ]
+	then
+		echo "-ldb"
+	fi
+fi
