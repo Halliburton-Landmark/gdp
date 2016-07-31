05392bbf2289675bd52778232bbdd17cf9fc4115
Eric Allman <eric@cs.berkeley.edu>
2016-07-31 12:34:11 -0700

    Avoid error message when compiled without git tree available.

--- adm/get-version-info.sh
+++ adm/get-version-info.sh
@@ -6,7 +6,7 @@
 
 cd `dirname $0`/..
 
-if type git > /dev/null 2>&1
+if type git > /dev/null 2>&1 && test -d .git
 then
 	# this will return the null string if we're not under git control
 	rev=`git rev-parse HEAD 2> /dev/null`
@@ -18,6 +18,8 @@ else
 	mods=""
 fi
 dtime=`date +'%Y-%m-%d %H:%M'`
+
+# add a leading space
 test -z "$rev" || rev=" $rev"
 
 echo "($dtime)$rev$mods"
