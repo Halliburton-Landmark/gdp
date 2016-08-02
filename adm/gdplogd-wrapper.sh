ec2303b9070ce18fde9f5796b570547050dcfb52
Eric Allman <eric@cs.berkeley.edu>
2016-08-01 18:24:36 -0700

    At least for now, turn on at least some gdplogd debugging by default.

--- adm/gdplogd-wrapper.sh
+++ adm/gdplogd-wrapper.sh
@@ -9,7 +9,7 @@
 # configure defaults
 : ${GDP_ROOT:=/usr}
 : ${GDP_LOG_DIR:=/var/log/gdp}
-: ${GDPLOGD_ARGS:=}
+: ${GDPLOGD_ARGS:="-D*=10"}
 : ${GDPLOGD_BIN:=$GDP_ROOT/sbin/gdplogd}
 : ${GDPLOGD_LOG:=$GDP_LOG_DIR/gdplogd.log}
 
