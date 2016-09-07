4706dee579dc0b0b7b2ed2b60881e6600acf063e
Zana Jipsen <zjipsen@berkeley.edu>
2016-09-07 13:30:54 -0700

    added javascript so the dropdown list is not only one long list of sensors

--- uhb/visualization/static/main.js
+++ uhb/visualization/static/main.js
@@ -40,15 +40,88 @@ deviceData = {
             { "logname": "edu.berkeley.eecs.swarmlab.device.c098e530007c", "type": "BLEES", "humanName": 'immersion BLEE: Chair 12'} ],
     };
 
+deviceCategories = { //DEPENDENT
+    "cats" : [
+            { "category": "type", "id": 1},
+            { "category": "office", "id": 2},
+//            { "category": "room", "id": 3},
+//            { "category": "university", "id": 4}
+], };
+
+office = {
+    "offices" : [
+         { "officename" : "bwrc" },
+         { "officename" : "swarmlab" }
+], };
+
+typelist = {
+    "types" : [
+         { "type" : "BLEES" },
+         { "type" : "Blink" },
+         { "type" : "PowerBlade" },
+], };
+
+function selectSubcats(doc) {
+    var subcatPickerString = "";
+    var e = document.getElementById('category'); //dependent
+    var selection = e.options[e.selectedIndex].value;
+    if (selection == 1) {
+    //sort by type
+	 for (var i=0;i<typelist.types.length;i++) {
+            var subcat = typelist.types[i];
+            subcatPickerString += "<option value='" + subcat.type + "'>" + subcat.type + "</option>";
+         }
+    }
+    if (selection == 2) {
+    //sort by office location (swarmlab, bwrc)
+         for (var i=0;i<office.offices.length;i++) {
+            var subcat = office.offices[i];
+            subcatPickerString += "<option value='" + subcat.officename + "'>" + subcat.officename + "</option>";
+            }
+    } 
+    doc.innerHTML=subcatPickerString;
+}
+
+function selectLogs(doc) {
+    var devicePickerString = ""; 
+    var e = document.getElementById('subcat'); //dependent
+    var f = document.getElementById('category');
+    var cat = f.options[f.selectedIndex].value;
+
+    for (var i=0; i<deviceData.logs.length; i++) {
+        var log = deviceData.logs[i];
+        if (cat == 1) { 
+            if (log.type == e.options[e.selectedIndex].value) { //DEPENDENT
+                devicePickerString += "<option value='" + log.logname + "'>" + log.humanName + "</option>"; 
+            }
+        }   
+        if (cat == 2) {
+            if (log.logname.includes(e.options[e.selectedIndex].value)) {
+                devicePickerString += "<option value='" + log.logname + "'>" + log.humanName + "</option>"; 
+            }
+        }
+    }
+    doc.innerHTML = devicePickerString;
+} 
 
 function makeForm() {
     // Code for picking up a device
-    var devicePickerString = "";
-    for (var i=0; i<deviceData.logs.length; i++) {
-        var log = deviceData.logs[i];
-        devicePickerString += "<option value='" + log.logname + "'>" + log.humanName + "</option>";
+    var catPickerString = ""; //DEPENDENT
+    for (var j=0; j<deviceCategories.cats.length; j++) {
+        var cat = deviceCategories.cats[j];
+        catPickerString += "<option value='" + cat.id + "'>" + cat.category + "</option>";
     }
-    document.getElementById('logname').innerHTML = devicePickerString;
+    document.getElementById('category').innerHTML = catPickerString;
+    selectSubcats(document.getElementById('subcat'));
+    selectLogs(document.getElementById('logname'));
+    document.getElementById('category').onchange=function(){
+        selectSubcats(document.getElementById('subcat'));
+	selectLogs(document.getElementById('logname'));
+    }
+    document.getElementById('subcat').onchange=function(){
+        selectLogs(document.getElementById('logname'));
+    }
+	
     var timeNow = Date.now()
     document.getElementById('startTime').value = new Date(timeNow-3600000).toLocaleString();
     document.getElementById('endTime').value = new Date(timeNow).toLocaleString();
