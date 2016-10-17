0332eadb7e2f556db9e83ebb2469b480c59be909
Nitesh Mor <mor.nitesh@gmail.com>
2016-10-17 11:28:02 -0700

    bokeh-vis: move logs to separate JS file for now

---
+++ uhb/bokeh-vis/static/logs.js
@@ -0,0 +1,130 @@
+deviceData = {
+        "logs": [
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e5300003", "type": "BLEES", "humanName": 'immersion BLEE: rear camera pillar'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e570002b", "type": "PowerBlade", "humanName": 'immersion PowerBlade: primary projector'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e570008b", "type": "PowerBlade", "humanName": 'immersion PowerBlade: secondary projector'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e5700088", "type": "PowerBlade", "humanName": 'immersion PowerBlade: audience camera'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e570008e", "type": "PowerBlade", "humanName": 'immersion PowerBlade: speaker camera'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e590000a", "type": "Blink", "humanName": 'immersion Blink: front wall adjacent to screen'},
+
+
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e530000a", "type": "BLEES", "humanName": 'swarm BLEE: inside entrance lobby'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e5900019", "type": "Blink", "humanName": 'swarm Blink: exit stair door near 490'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e590000b", "type": "Blink", "humanName": 'swarm Blink: inside entrance lobby'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e5900091", "type": "Blink", "humanName": 'swarm Blink: 4th floor elevator lobby'},
+
+
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e5300009", "type": "BLEES", "humanName": 'bwrc BLEE: lab rear wall'},
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e5300036", "type": "BLEES", "humanName": 'bwrc BLEE: rear wall'},
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e5300054", "type": "BLEES", "humanName": 'bwrc BLEE: hot aisle'},
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e530005d", "type": "BLEES", "humanName": 'bwrc BLEE: mid wall'},
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e570008f", "type": "PowerBlade", "humanName": 'bwrc PowerBlade: utility power to sump pump'},
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e5700090", "type": "PowerBlade", "humanName": 'bwrc PowerBlade: power to air compressor'},
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e590001e", "type": "Blink", "humanName": 'bwrc Blink: server room'}, 
+
+//added sept. 21: bwrc office area
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e53000a5", "type": "BLEES", "humanName": 'bwrc BLEE: main seating area'},
+//            { "logname": "edu.berkeley.eecs.bwrc.device.c098e53000a3", "type": "BLEES", "humanName": 'bwrc BLEE: conference room (rabaey)'},
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e53000a4", "type": "BLEES", "humanName": 'bwrc BLEE: admin office'},
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e53000a2", "type": "BLEES", "humanName": 'bwrc BLEE: conference room (brodersen)'},
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e53000a1", "type": "BLEES", "humanName": 'bwrc BLEE: ken\'s office'},
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e53000a0", "type": "BLEES", "humanName": 'bwrc BLEE: brian office above thermostat'},
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e5300037", "type": "BLEES", "humanName": 'bwrc BLEE: center of student seating area'},
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e5900094", "type": "Blink", "humanName": 'bwrc Blink: rabaey conference room'}, 
+            { "logname": "edu.berkeley.eecs.bwrc.device.c098e5900087", "type": "Blink", "humanName": 'bwrc Blink: broderson conference room'}, 
+
+
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e530002b", "type": "BLEES", "humanName": 'immersion BLEE: Chair 1'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e5300058", "type": "BLEES", "humanName": 'immersion BLEE: Chair 2'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e530005c", "type": "BLEES", "humanName": 'immersion BLEE: Chair 3'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e530005f", "type": "BLEES", "humanName": 'immersion BLEE: Chair 4'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e5300074", "type": "BLEES", "humanName": 'immersion BLEE: Chair 5'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e5300075", "type": "BLEES", "humanName": 'immersion BLEE: Chair 6'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e5300076", "type": "BLEES", "humanName": 'immersion BLEE: Chair 7'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e5300077", "type": "BLEES", "humanName": 'immersion BLEE: Chair 8'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e5300078", "type": "BLEES", "humanName": 'immersion BLEE: Chair 9'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e530007a", "type": "BLEES", "humanName": 'immersion BLEE: Chair 10'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e530007b", "type": "BLEES", "humanName": 'immersion BLEE: Chair 11'},
+            { "logname": "edu.berkeley.eecs.swarmlab.device.c098e530007c", "type": "BLEES", "humanName": 'immersion BLEE: Chair 12'},
+
+//added sept. 21: jacobs hall, TAMU
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5700113", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: 3D printer 10026 rm 110'},
+//            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5700122", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: 3D printer 10026B rm 110'},
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5700123", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: 3D printer 10047 rm 110'}, 
+//            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5700125", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: 3D printer 10047 rm 110'},           
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5700126", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: Laser 2 rm 110C'},
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5700127", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: 3D printer 10097 rm 110'},
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5700129", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: 3D printer 10213 rm 110'},
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e57000e2", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: Laser 1 rm 110C'},
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e57000e5", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: 3D printer 10035 rm 110'},
+//            { "logname": "edu.berkeley.eecs.jacobs.device.c098e57000d3", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: 3D printer 10098 rm 110'},
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e57000d7", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: 3D printer 10025 rm 110'},
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e57000f7", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: 3D printer 10038 rm 110'},
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e57000db", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: 3D printer 10094 rm 110'},
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5700131", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: 3D printer 10034 rm 110'},
+
+//            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5300057", "type": "BLEES", "humanName": 'jacobs BLEE: rm 110 by thermostat'},
+//            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5300055", "type": "BLEES", "humanName": 'jacobs BLEE: rm 110 check placement in room'},
+
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e570008a", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: Carbon 3D printer rm 332'},
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e570010c", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: Dim. 2 (Bonnie) rm 332'},
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e570012e", "type": "PowerBlade", "humanName": 'jacobs PowerBlade: Dim. 1 (Clyde) rm 332'},
+
+//            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5300055", "type": "BLEES", "humanName": 'jacobs BLEE: rm 120'},
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5300050", "type": "BLEES", "humanName": 'jacobs BLEE: rm 332 Fortus 380 air exhaust vent'},
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5300051", "type": "BLEES", "humanName": 'jacobs BLEE: rm 332 center of room'},
+
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5900085", "type": "Blink", "humanName": 'jacobs Blink: rm 332'}, 
+//            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5900094", "type": "Blink", "humanName": 'jacobs Blink: name unknown1'},
+//            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5900087", "type": "Blink", "humanName": 'jacobs Blink: name unknown2'}, 
+
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5900081", "type": "Blink", "humanName": 'jacobs Blink: rm 10C woodshop'}, 
+
+            { "logname": "edu.berkeley.eecs.jacobs.device.c098e5300062", "type": "BLEES", "humanName": 'jacobs BLEE: rm 10C woodshop'},
+
+
+            { "logname": "edu.tamu.esp.Setup1c.device.c098e530001d", "type": "BLEES", "humanName": 'TAMU BLEE'}, 
+//            { "logname": "edu.tamu.esp.Setup1c.device.c098e590010", "type": "Blink", "humanName": 'TAMU Blink1'}, 
+//            { "logname": "edu.tamu.esp.Setup1c.device.c098e530011", "type": "Blink", "humanName": 'TAMU Blink2'}, 
+//            { "logname": "edu.tamu.esp.Setup1c.device.c098e530012", "type": "Blink", "humanName": 'TAMU Blink3'}, 
+//            { "logname": "edu.tamu.esp.Setup1c.device.c098e5300ac", "type": "Blink", "humanName": 'TAMU Blink4'}, 
+
+//            { "logname": "edu.tamu.esp.Setup1c.device.c098e5700028", "type": "PowerBlade", "humanName": 'TAMU PowerBlade: unknown1'}, 
+            { "logname": "edu.tamu.esp.Setup1c.device.c098e57000c4", "type": "PowerBlade", "humanName": 'TAMU PowerBlade: unknown2'}, 
+            { "logname": "edu.tamu.esp.Setup1c.device.c098e57000a8", "type": "PowerBlade", "humanName": 'TAMU PowerBlade: unknown3'}, 
+
+            { "logname": "edu.berkeley.eecs.swarmlab.device.00124b000a64aa04", "type": "i3mote", "humanName": 'swarmlab i3mote-1'}, 
+            { "logname": "edu.berkeley.eecs.swarmlab.device.00124b000a55f706", "type": "i3mote", "humanName": 'swarmlab i3mote-2'}, 
+            // { "logname": "edu.berkeley.eecs.swarmlab.device.00124b0007907583", "type": "i3mote", "humanName": 'swarmlab i3mote-3'}, 
+            { "logname": "edu.berkeley.eecs.swarmlab.device.00124b000a4c5880", "type": "i3mote", "humanName": 'swarmlab i3mote-4'}, 
+            { "logname": "edu.berkeley.eecs.swarmlab.device.00124b000d7dfa07", "type": "i3mote", "humanName": 'swarmlab i3mote-5'}, 
+            // { "logname": "edu.berkeley.eecs.swarmlab.device.00124b000d7dfb87", "type": "i3mote", "humanName": 'swarmlab i3mote-6'}, 
+            // { "logname": "edu.berkeley.eecs.swarmlab.device.00124b0007910e03", "type": "i3mote", "humanName": 'swarmlab i3mote-7'}, 
+
+],    };
+
+deviceCategories = { //DEPENDENT
+    "cats" : [
+            { "category": "place", "id": 2},
+            { "category": "type", "id": 1},
+//            { "category": "room", "id": 3},
+//            { "category": "university", "id": 4}
+], };
+
+place = {
+    "offices" : [
+         { "officename" : "bwrc" },
+         { "officename" : "swarmlab" },
+         { "officename" : "jacobs" },
+         { "officename" : "tamu" }
+], };
+
+typelist = {
+    "types" : [
+         { "type" : "BLEES" },
+         { "type" : "Blink" },
+         { "type" : "PowerBlade" },
+         { 'type' : 'i3mote'},
+], };
+
+
