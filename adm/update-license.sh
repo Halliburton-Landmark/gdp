987041eaac4e09b552b704c5efb5ef0d8c64e22c
Eric Allman <eric@cs.berkeley.edu>
2017-02-07 19:12:18 -0800

    Allow update-license to use LICENSE files in .. if none found in .

--- adm/update-license.sh
+++ adm/update-license.sh
@@ -13,10 +13,18 @@ update_license() {
 	    echo WARNING: could not update license for $file 1>&2
 }
 
+#  Fudge LICENSE file in this directory or parent, no further up
+rmLICENSE=false
 if [ ! -r LICENSE ]
 then
-	echo "[ERROR] LICENSE file must exist"
-	exit 1
+	if [ -r ../LICENSE ]
+	then
+		ln -s ../LICENSE
+		rmLICENSE=true
+	else
+		echo "[ERROR] LICENSE file must exist"
+		exit 1
+	fi
 fi
 
 
@@ -25,3 +33,10 @@ do
 	echo Updating $f
 	update_license $f
 done
+
+if $rmLICENSE
+then
+	rm LICENSE
+fi
+
+exit 0
