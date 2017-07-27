f3360cb5eb6dda48cb7076840a9aaa7ed59b86cb
Eric Allman <eric@cs.berkeley.edu>
2017-07-27 10:12:54 -0700

    Change startup wrappers to return status of target program, not status of llogger.  This makes them more systemd friendly (in particular for restarts).

--- adm/gdp-rest-wrapper.sh
+++ adm/gdp-rest-wrapper.sh
@@ -9,12 +9,62 @@
 # configure defaults
 : ${GDP_ROOT:=/usr}
 : ${GDP_LOG_DIR:=/var/log/gdp}
+: ${GDP_USER:=gdp}
 : ${GDP_REST_ARGS:="-D*=10"}
 : ${GDP_REST_BIN:=$GDP_ROOT/sbin/gdp-rest}
 : ${GDP_REST_LOG:=$GDP_LOG_DIR/gdp-rest.log}
-: ${LLOGGER:=llogger}
+: ${LLOGGER:="llogger -s3"}
 
-{
-	echo `date +"%F %T %z"` Running $GDP_REST_BIN $GDP_REST_ARGS
-	exec $GDP_REST_BIN $GDP_REST_ARGS
-} 2>& 1 | ${LLOGGER} -a $GDP_REST_LOG
+# manifest constants (see <sysexits.h>)
+EX_USAGE=64
+EX_UNAVAILABLE=69
+EX_NOPERM=77
+EX_CONFIG=78
+
+# if we are running as root, start over as gdp
+test `whoami` = "root" && exec sudo -u $GDP_USER $0 "$@"
+
+debug=false
+if [ "x$1" = "x-D" ]
+then
+	debug=true
+	shift
+fi
+
+if ! $debug
+then
+	# make sure log file exists so we can append to it
+	test -f $GDP_REST_LOG || cp /dev/null $GDP_REST_LOG
+	fifo=/tmp/gdp-logger.$$
+	cleanup() {
+		rm $fifo
+	}
+	trap cleanup EXIT
+	if ! mkfifo -m 600 $fifo
+	then
+		echo "[FATAL] Cannot create fifo $fifo"
+		exit $EX_NOPERM
+	fi
+
+	${LLOGGER} -a $GDP_REST_LOG < $fifo &
+
+	# redirect remaining output to the log
+	exec > $fifo 2>&1
+fi
+
+echo "[INFO] Running $0 $@ as `whoami`"
+if [ `whoami` != $GDP_USER ]
+then
+	echo "[WARN] Should be running as $GDP_USER"
+fi
+if [ $# -ne 0 ]
+then
+	echo "[ERROR] Usage: $0 [-D]"
+	exit $EX_USAGE
+fi
+
+echo "[INFO] Running $GDP_REST_BIN $GDP_REST_ARGS"
+$GDP_REST_BIN $GDP_REST_ARGS
+rc=$?
+echo "[FATAL] $0: $GDP_REST_BIN exited with status $rc"
+exit $rc
--- adm/gdplogd-wrapper.sh
+++ adm/gdplogd-wrapper.sh
@@ -9,12 +9,62 @@
 # configure defaults
 : ${GDP_ROOT:=/usr}
 : ${GDP_LOG_DIR:=/var/log/gdp}
+: ${GDP_USER:=gdp}
 : ${GDPLOGD_ARGS:="-D*=10"}
 : ${GDPLOGD_BIN:=$GDP_ROOT/sbin/gdplogd}
 : ${GDPLOGD_LOG:=$GDP_LOG_DIR/gdplogd.log}
-: ${LLOGGER:=llogger}
+: ${LLOGGER:="llogger -s3"}
 
-{
-	echo `date +"%F %T %z"` Running $GDPLOGD_BIN $GDPLOGD_ARGS
-	exec $GDPLOGD_BIN $GDPLOGD_ARGS
-} 2>& 1 | ${LLOGGER} -a $GDPLOGD_LOG
+# manifest constants (see <sysexits.h>)
+EX_USAGE=64
+EX_UNAVAILABLE=69
+EX_NOPERM=77
+EX_CONFIG=78
+
+# if we are running as root, start over as gdp
+test `whoami` = "root" && exec sudo -u $GDP_USER $0 "$@"
+
+debug=false
+if [ "x$1" = "x-D" ]
+then
+	debug=true
+	shift
+fi
+
+if ! $debug
+then
+	# make sure log file exists so we can append to it
+	test -f $GDPLOGD_LOG || cp /dev/null $GDPLOGD_LOG
+	fifo=/tmp/gdp-logger.$$
+	cleanup() {
+		rm $fifo
+	}
+	trap cleanup EXIT
+	if ! mkfifo -m 600 $fifo
+	then
+		echo "[FATAL] Cannot create fifo $fifo"
+		exit $EX_NOPERM
+	fi
+
+	${LLOGGER} -a $GDPLOGD_LOG < $fifo &
+
+	# redirect remaining output to the log
+	exec > $fifo 2>&1
+fi
+
+echo "[INFO] Running $0 $@ as `whoami`"
+if [ `whoami` != $GDP_USER ]
+then
+	echo "[WARN] Should be running as $GDP_USER"
+fi
+if [ $# -ne 0 ]
+then
+	echo "[ERROR] Usage: $0 [-D]"
+	exit $EX_USAGE
+fi
+
+echo "[INFO] Running $GDPLOGD_BIN $GDPLOGD_ARGS"
+$GDPLOGD_BIN $GDPLOGD_ARGS
+rc=$?
+echo "[FATAL] $0: $GDPLOGD_BIN exited with status $rc"
+exit $rc
