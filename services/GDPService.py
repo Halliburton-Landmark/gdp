20f950f6506fe5a9075949976650e112e003db71
Nitesh Mor <mor.nitesh@gmail.com>
2017-08-29 22:57:12 -0700

    First version of log-creation service
    - does not support advertising for multiple addresses
    - does not quite yet pick up from last run

---
+++ services/GDPService.py
@@ -0,0 +1,408 @@
+#!/usr/bin/env python
+
+"""                                                                             
+
+A library to perform network related operations (setting up TCP connections,
+parsing incoming PDUs, sending out replies, etc) for a GDP service.
+                                                                               
+To use GDPService for your own service, at the very minimum, you need to
+subclass GDPService and override the method 'request_handler'. Then, you need
+to create an instance of this service with the 256-bit GDP address that this
+service should listen on and the address of a GDP router (as a
+"IP-address:port" string). 
+                                                                                
+Any GDP PDU destined to the GDP address specified for at the instantiation time
+is parsed and passed as a dictionary to the 'request_handler'. This request
+handler should act on the received data and return a python dictionary
+containing field=>value pairs for a PDU to be sent as a reply. GDPService adds
+some defaults (such as source address, destination address, etc) unless these
+are overridden in the response dictionary.
+
+"""
+
+from twisted.internet.protocol import ClientFactory, Protocol
+from twisted.internet import reactor
+import threading
+import struct
+import random
+import argparse
+import threading
+
+
+def pprint(d):
+    """
+    Just our own pprint function that behaves nicely with binary strings
+    """
+
+    def to_hex(data):
+        hex_repr = "".join([hex(ord(t))[2:] for t in data])
+        if len(data)>0: hex_repr = '\\x' + hex_repr
+        return hex_repr
+
+    print "{"
+    for k in d.keys():
+        tmp = d[k]
+        if type(tmp) == type(0): p = str(tmp)
+        else: p = "'" + to_hex(tmp) + "'"
+        print " '" + k + "': " + p + ","
+    print "}"
+
+
+class GDPProtocol(Protocol):
+
+    """ An instance of this class is created for every connection """
+
+    # this is the conventional GDP address of a router
+    GDPROUTER_ADDRESS = (chr(255) + chr(0)) * 16
+
+    def __init__(self, GDPaddress, args):
+        """ The GDP address for the service end point """
+
+        self.GDPaddress = GDPaddress
+        self.args = args
+        self.buffer = ""
+
+    def connectionMade(self):
+
+        # Send the advertisement message
+        self.advertisement = ('\x03' + '\x00' * 2 + '\x01' +
+                            self.GDPROUTER_ADDRESS + self.GDPaddress +
+                            '\x00' * 4 + '\x00' * 4 + '\x00'*4 )
+
+        ## do network I/O in the reactor thread
+        reactor.callFromThread(self.transport.write, self.advertisement)
+
+
+    def terminateConnection(self, reason):
+        """
+        If we can't make sense out of any data on this connection
+        anymore for any reason, terminate the connection.
+        """
+
+        # This just terminates the connection
+        self.transport.abortConnection()
+
+
+    def dataReceived(self, data):
+
+        """
+        parses PDUs and invokes the PDU processing code in a separate
+        worker thread
+        """
+
+        assert threading.currentThread().getName() == "ReactorThr"
+
+        # Make sure we have some flow control.
+
+        l1 = len(self.buffer)   # what is left from previous calls
+        l2 = len(data)          # what we got now
+        l = 0                   # seek pointer.  0 <= l < l1+l2
+
+        while reactor.running:
+
+            def __get_byte_num(n):
+                """ Reference to bit number 'l+n' in self.buffer + data,
+                    0-indexed """
+                if l + n < l1:
+                    return self.buffer[l + n]
+                else:
+                    return data[l + n - l1]
+
+            if (l + 1) > (l1 + l2):
+                break       # can't figure out the version
+
+            # check the version number
+            version = __get_byte_num(0)
+
+            if version != '\x03':   # bogus version number
+                self.terminateConnection("bogus version number")
+                break
+
+            if (l + 80) > (l1 + l2):
+                # incomplete header for the current message
+                # we need at least 80 bytes to figure out the length of
+                #   this PDU
+                break
+
+            # calculate lengths we need to find if we received full PDU
+            sig_len = (ord(__get_byte_num(72)) & 0x0f) * 256 + \
+                            ord(__get_byte_num(73))
+            opt_len = 4 * ord(__get_byte_num(74))
+
+            len_arr = [__get_byte_num(idx) for idx in xrange(76, 80)]
+            data_len = 0
+            for __t in len_arr:
+                data_len = data_len * 256 + ord(__t)
+
+            # pdu_len is the total length of the PDU
+            pdu_len = 80 + opt_len + data_len + sig_len
+
+            if (l + pdu_len) > (l1 + l2):
+                break     # incomplete message
+
+            # 3 cases:
+            # - entire message in self.buffer
+            # - message split between self.buffer and data
+            # - entire message in data
+            # case 2 can only happen once every call of this function
+
+            message = None
+            if (l + pdu_len) <= l1:  # case 1
+                message = buffer(self.buffer, l, pdu_len)
+            elif l >= l1:         # case 3
+                message = buffer(data, l - l1, pdu_len)
+            else:               # case 2
+                # This is not a buffer anymore because of concatenation
+                message = (buffer(self.buffer, l, l1 - l) +
+                           buffer(data, 0, (pdu_len) - (l1 - l)))
+
+            l = l + pdu_len       # update l
+
+            # Work on this PDU, all the PDU processing logic goes here;
+            # this gets called in a separate worker thread.
+            reactor.callInThread(self.process_PDU,
+                                    message, opt_len, sig_len, data_len)
+
+            # End of while loop
+
+        if reactor.running:
+
+            # Now update the buffer with the remaining data
+            # 2 cases: 1) Either the new buffer is a substring of just data
+            # OR, 2) it is substring of old buffer + all data
+
+            if l > l1:    # case 1
+                self.buffer = buffer(
+                    data, l - l1, (l1 + l2) - l)    # just a buffer
+            else:       # case 2
+                self.buffer = (buffer(self.buffer, l, l1 - l) +
+                               buffer(data, 0, len(data)))  # Expensive string
+            # if len(self.buffer)>0: assert ord(self.buffer[0])==2
+
+
+    def process_PDU(self, message, opt_len, sig_len, data_len):
+        """
+        Act on a PDU that is already parsed. All the PDU processing
+        logic goes here.
+
+        Note that the message could be a string or a buffer. For now, it
+        just parses it into a dictionary, calls the request request
+        handler on the parsed dictionary and sends the returned python
+        dictionary to the provided destination
+        """
+
+        # DEBUG-info:
+        # 'message' is a PDU received by gdp_router from remote
+        # address 'self.transport.getPeer()'
+
+        # create a dictionary
+        msg_dict = {}
+        msg_dict['fmt'] = message[0]
+        msg_dict['ttl'] = ord(message[1])
+        msg_dict['res'] = message[2]
+        msg_dict['cmd'] = ord(message[3])
+
+        msg_dict['dst'] = message[4:36]
+        msg_dict['src'] = message[36:68]
+
+        msg_dict['rid'] = struct.unpack("!I", message[68:72])[0]
+
+        msg_dict['sigalg'] = ord(message[72])>>4
+        msg_dict['siglen'] = (ord(message[72]) & 0x0f) * 256 + ord(message[73])
+        msg_dict['siginfo'] = message[72:74]
+        assert msg_dict['siglen'] == sig_len
+
+        msg_dict['olen'] = 4*ord(message[74])
+        assert msg_dict['olen'] == opt_len
+
+        msg_dict['flags'] = message[75]
+
+        msg_dict['dlen'] = struct.unpack("!I", message[76:80])[0]
+        assert msg_dict['dlen'] == data_len
+
+        ctr = 80
+        if msg_dict['olen'] != 0:
+            msg_dict['options'] = message[ctr:ctr+msg_dict['olen']]
+        ctr += msg_dict['olen']
+
+        msg_dict['data'] = message[ctr:80+ctr+msg_dict['dlen']]
+        ctr += msg_dict['dlen']
+
+        msg_dict['sig'] = message[ctr:]
+
+        # get the response dictionary
+        # msg_dictuest_handler is to be supplied by GDPProtocolFactory, which
+        #   in turn gets it from GDPService
+        # XXX: What all information should be made available here?
+        keys = set(msg_dict.keys()) & set(['cmd', 'dst', 'src', 'flags',
+                                                'rid', 'data', 'options'])
+        data = {k: msg_dict[k] for k in keys}
+        if self.args.get("debug", False):
+            print "GDPService: Passing to request handler..."
+            pprint(data)
+        resp = self.request_handler(data)
+        if self.args.get("debug", False):
+            print "GDPService: Received from request handler..."
+            pprint(resp)
+
+        if resp is not None:
+            # if a destination is specified in the response, it will take
+            #   precedence
+            self.send_PDU(resp, dst=msg_dict['src'])
+
+
+    def request_handler(self, req):
+        pass
+
+
+    def send_PDU(self, data, **kwargs):
+        """
+        Create a GDP message and write it to the network connection
+        destination is a 256 bit address. This should *not* be called
+        from the reactor thread.
+
+        Qpen question: what all fields should be accepted from the caller?
+        So far: cmd, dst, src, flags, rid, data, options
+        """
+
+        assert threading.currentThread().getName() != "ReactorThr"
+
+        msg = {}
+        msg['fmt'] = '\x03'
+        msg['ttl'] = chr(0)                     # FIXME
+        msg['res'] = '\x00'
+        msg['cmd'] = chr(data['cmd'])           # mandatory
+
+        # make sure we do have a destination
+        assert 'dst' in data.keys() + kwargs.keys()
+        msg['dst'] = str(data.get('dst', kwargs.get('dst', None)))
+        msg['src'] = str(data.get('src', self.GDPaddress))
+
+        # XXX: The following are from protocol ver 0x02
+        # These are replaced by siginfo
+        # msg['sigalg'] = '\x00'
+        # msg['siglen'] = chr(0)
+        msg['siginfo'] = '\x00\x00'
+
+        assert len(data.get('options', ''))%4 == 0
+        msg['olen'] = chr(len(data.get('options', ''))/4)
+        msg['flags'] = str(data.get('flags', '\x00'))
+
+        msg['rid'] = struct.pack("!I", data.get('rid', 0))
+
+        msg['dlen'] = struct.pack("!I", len(data.get('data', '')))
+
+        msg['options'] = str(data.get('options', ''))
+        msg['data'] = str(data.get('data', ''))
+        msg['sig'] = ''
+
+        # some assertions (all may not be be necessary)
+        valid_size = {  'fmt': 1, 'ttl': 1, 'res': 1, 'cmd': 1,
+                        'dst': 32, 'src': 32,
+                        'siginfo': 2, 'olen': 1, 'flags': 1,
+                        'rid': 4, 'dlen': 4}
+        for k in valid_size.keys():
+            assert len(msg[k]) == valid_size[k]
+
+        message = ( msg['fmt'] + msg['ttl'] + msg['res'] + msg['cmd'] +
+                    msg['dst'] + msg['src'] +
+                    msg['rid'] +
+                    msg['siginfo'] + msg['olen'] + msg['flags'] +
+                    msg['dlen'] +
+                    msg['options'] + msg['data'] + msg['sig'] )
+
+        ## the following gets called in the reactor thread
+        reactor.callFromThread(self.transport.write, message)
+
+
+
+class GDPProtocolFactory(ClientFactory):
+
+    def __init__(self, GDPaddress, args):
+        self.GDPaddress = GDPaddress
+        self.args = args
+
+    def buildProtocol(self, remoteaddr):
+        protocol = GDPProtocol(self.GDPaddress, self.args)
+        protocol.request_handler = self.request_handler
+        return protocol
+
+    def request_handler(self,req):
+        pass
+
+
+class GDPService(object):
+    """
+    A generic GDP Service. In a perfect world, all GDP services should
+    be subclasses of this class.
+
+    To implement your own GDP service, override the 'request_handler' method
+    and put your service's request processing logic.
+
+    One can also override 'setup' to do service specific startup functions.
+    These functions are called *before* establishing connection to the
+    GDP router.
+    """
+
+
+    def __init__(self, GDPaddress, router, **kwargs):
+        """
+        address: 256 bit GDP address
+        router: ip:port for a GDP router
+        """
+
+        ## parse the GDP router host:port
+        t = router.split(":")
+        self.router_host = t[0]
+        self.router_port = int(t[1])
+        self.GDPaddress = GDPaddress
+
+        self.kwargs = kwargs
+
+        ProtocolFactory = GDPProtocolFactory(GDPaddress, self.kwargs)
+        ProtocolFactory.request_handler = self.request_handler
+
+        ## Call service specific setup code
+        self.setup()
+
+        ## Establish connection to the router (the reactor isn't running
+        ## yet, so this will only get established when 'start()' is called.
+        if kwargs.get("debug", False):
+            print "Connecting to host:%s, port:%d" % \
+                    (self.router_host, self.router_port)
+        reactor.connectTCP(self.router_host, self.router_port, ProtocolFactory)
+
+
+    def setup(self):
+        "Initial setup for this service. Default: do nothing"
+        pass
+
+
+    def request_handler(self,req):
+        """
+        Receive a request (dictionary), with the following keys:
+            cmd = Command field in GDP PDU
+            src = Source GDP address (256 bit)
+            dst = Destination GDP address,
+            rid = Request ID
+            data = Actual payload
+        Ideally, a GDP service will override this method
+        """
+        pass
+
+
+    def start(self):
+        """
+        Start the reactor (in a separate thread) and returns immediately. One
+        should put appropriate logic after calling 'start' to keep main thread
+        alive as long as possible.
+        """
+        self.reactor_thr = threading.Thread(name="ReactorThr",
+                                 target=reactor.run,
+                                 kwargs={'installSignalHandlers':False})
+        self.reactor_thr.start()
+
+
+    def stop(self):
+        """ Stop serving requests, (ideally) called from main-thread """
+        reactor.callFromThread(reactor.stop)
---
+++ services/logCreationService.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+
+from GDPService import GDPService
+import gdp
+import random
+import cPickle
+import argparse
+import sys
+import time
+import sqlite3
+import logging
+import os
+
+GDP_SERVICE_ADDR = gdp.GDP_NAME("logcreationservice")
+GDP_LOG_NAME = "logcreationservicelog"
+GDP_LOG_ADDR = gdp.GDP_NAME(GDP_LOG_NAME)
+
+DEFAULT_ROUTER_PORT = 8007
+DEFAULT_ROUTER_HOST = "172.30.0.1"
+
+### these come from gdp/gdp_pdu.h from the GDP C library
+
+# Acks/Naks
+GDP_ACK_MIN = 128
+GDP_ACK_MAX = 191
+GDP_NAK_C_MIN = 192
+GDP_NAK_C_MAX = 223
+GDP_NAK_C_MIN = 223
+GDP_NAC_C_MAX = 239
+GDP_NAK_R_MIN = 240
+GDP_NAK_R_MAX = 254
+
+# specific commands
+GDP_CMD_CREATE = 66
+GDP_NAK_C_BADREQ = 192
+
+
+class logCreationService(GDPService):
+
+    def __init__(self, GDPaddress, router, logservers, dbname, **kwargs):
+        """
+        GDPaddress: the address of this particular service
+        router: a 'host:port' string representing the GDP router
+        logservers: a list of log-servers on the backend that we use
+        dbname: sqlite database location
+        """
+
+        ## First call the __init__ of GDPService
+        super(logCreationService, self).__init__(GDPaddress, router, **kwargs)
+
+        ## Setup instance specific constants
+        self.logservers = [gdp.GDP_NAME(x).internal_name() for x in logservers]
+        self.dbname = dbname
+
+        ## Setup a connection to the backend database
+        if os.path.exists(self.dbname):
+            logging.info("Loading existing database, %r", self.dbname)
+            self.conn = sqlite3.connect(self.dbname, check_same_thread=False)
+            self.cur = self.conn.cursor()
+        else:
+            logging.info("Creating new database, %r", self.dbname)
+            self.conn = sqlite3.connect(self.dbname, check_same_thread=False)
+            self.cur = self.conn.cursor()
+
+            ## Make table for bookkeeping
+            self.cur.execute("""CREATE TABLE logs(
+                                    logname BLOB UNIQUE, srvname BLOB,
+                                    ack_seen INTEGER DEFAULT 0, 
+                                    ts DATETIME DEFAULT CURRENT_TIMESTAMP,
+                                    creator BLOB, rid INTEGER)""")
+            self.cur.execute("""CREATE UNIQUE INDEX logname_ndx
+                                                ON logs(logname)""")
+            self.cur.execute("CREATE INDEX srvname_ndx ON logs(srvname)")
+            self.cur.execute("CREATE INDEX ack_seen_ndx ON logs(ack_seen)")
+            self.conn.commit()
+
+
+    def __del__(self):
+
+        ## Close database connections
+        logging.info("Closing database connection to %r", self.dbname)
+        self.conn.close()
+
+
+    def request_handler(self, req):
+        """
+        The routine that gets invoked when a PDU is received. In our case,
+        it can be either a new request (from a client), or a response from
+        a log-server.
+        """
+
+        # check if it's a request from a client or a response from a logd.
+
+        if req['cmd']<128:      ## it's a command
+
+            ## First check for any error conditions. If any of the
+            ## following occur, we ought to send back a NAK
+
+            if req['src'] in self.logservers:
+                logging.info("error: received cmd %d from server", req['cmd'])
+                return self.gen_bad_request_resp(req)
+
+            if req['cmd'] != GDP_CMD_CREATE:
+                logging.info("error: recieved unknown request")
+                return self.gen_bad_request_resp(req)
+
+            logging.info("Received Create request from a client")
+
+            ## figure out the data we need to insert in the database
+            logname = req['data'][:32]
+            srvname = random.choice(self.logservers)
+            ## private information that we will need later
+            creator = req['src']
+            rid = req['rid']
+
+            ## log this to our backend database
+            try:
+                logging.debug("inserting to database %r, %r, %r, %d",
+                                logname, srvname, creator, rid)
+                self.cur.execute("""INSERT INTO logs (logname, srvname,
+                                    creator, rid) VALUES(?,?,?,?);""",
+                                    (sqlite3.Binary(logname),
+                                     sqlite3.Binary(srvname),
+                                     sqlite3.Binary(creator), rid))
+                self.conn.commit()
+
+            except sqlite3.IntegrityError:
+
+                logging.info("Log already exists")
+                return self.gen_bad_request_resp(req)
+
+            ## Send a spoofed request to the logserver
+            spoofed_req = req
+            spoofed_req['src'] = self.GDPaddress
+            spoofed_req['dst'] = srvname
+            spoofed_req['rid'] = self.cur.lastrowid
+
+            # now return this spoofed request back to the transport layer
+            # Since we have overridden the destination, it will go
+            # to a log server instead of the actual client
+            return spoofed_req
+
+        else: ## response.
+
+            ## Sanity checking
+            if req['src'] not in self.logservers:
+                logging.info("error: received a non-response from logserver")
+                return self.gen_bad_request_resp(req) 
+
+            logging.info("Received response from a log-server")
+
+            ## Fetch the original creator and rid from our database
+            self.cur.execute("""SELECT creator, rid, ack_seen FROM logs
+                                            WHERE rowid=?""", (req['rid'],))
+            dbrows = self.cur.fetchall()
+
+            good_resp = len(dbrows)==1
+            if good_resp:
+                (creator, orig_rid, ack_seen) = dbrows[0]
+                if ack_seen != 0:
+                    good_resp = False
+
+            if not good_resp:
+                logging.info("error: bogus response")
+                return self.gen_bad_request_resp(req)
+            else:
+                logging.info("Setting ack_seen to 1 for row %d", req['rid'])
+                self.cur.execute("""UPDATE logs SET ack_seen=1
+                                            WHERE rowid=?""", (req['rid'],))
+                self.conn.commit()
+
+            # create a spoofed reply and send it to the client
+            spoofed_reply = req
+            spoofed_reply['src'] = self.GDPaddress
+            spoofed_reply['dst'] = creator
+            spoofed_reply['rid'] = orig_rid
+
+            # return this back to the transport layer
+            return spoofed_reply
+
+
+    def gen_bad_request_resp(self, req):
+        resp = dict()
+        resp['cmd'] = GDP_NAK_C_BADREQ
+        resp['src'] = self.GDPaddress
+        resp['dst'] = req['src']
+        return resp
+
+
+if __name__ == "__main__":
+
+    ## argument parsing
+    parser = argparse.ArgumentParser(description="Log creation service")
+
+    parser.add_argument("-D", "--debug", action='store_true',
+                                help="Turn on debugging")
+    parser.add_argument("-i", "--host", type=str, default=DEFAULT_ROUTER_HOST,
+                                help="host of gdp_router instance. "
+                                     "default = %s" % DEFAULT_ROUTER_HOST)
+    parser.add_argument("-p", "--port", type=int, default=DEFAULT_ROUTER_PORT,
+                                help="port for gdp_router instance. "
+                                     "default = %d" % DEFAULT_ROUTER_PORT)
+    parser.add_argument("dbname", type=str, 
+                                help="filename for sqlite database")
+    parser.add_argument("logservers", type=str, nargs="+",
+                                help="log daemons that this instance of "
+                                     "log creation service should use")
+
+    args = parser.parse_args()
+
+    ## done argument parsing, instantiate the service
+    logging.basicConfig(level=logging.DEBUG)
+    service = logCreationService(GDP_SERVICE_ADDR.internal_name(),
+                                    "%s:%d" % (args.host, args.port),
+                                    args.logservers, args.dbname,
+                                    debug=args.debug)
+
+    ## all done, start the service (and sleep indefinitely)
+    logging.info("Starting logcreationservice")
+    service.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except (KeyboardInterrupt, SystemExit):
+        service.stop()
