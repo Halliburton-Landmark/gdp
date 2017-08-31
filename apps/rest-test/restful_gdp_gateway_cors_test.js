2b8d706bea87c5100aa57d978df44fcb0cd24f56
Rick Pratt <rpratt@berkeley.edu>
2017-08-31 09:54:28 -0700

    Created a GCL specifically for testing CORS, adjusted .js to use it.

--- apps/rest-test/restful_gdp_gateway_cors_test.js
+++ apps/rest-test/restful_gdp_gateway_cors_test.js
@@ -1,8 +1,26 @@
+// bash-3.2$ ./restful_gcl_create.py 
+//
+// ==== PUT GCL Response Page:
+// STATUS:
+// 201
+// HEADERS:
+// {'Access-Control-Allow-Methods': 'GET, POST, PUT, OPTIONS', 'Access-Control-Max-Age': '86400', 'Transfer-Encoding': 'chunked', 'Server': 'lighttpd/1.4.35', 'Date': 'Thu, 31 Aug 2017 16:21:49 GMT', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Authorization, Content-Type', 'Content-Type': 'application/json'}
+// CONTENT:
+// {
+//     "gdplogd_name": "edu.berkeley.eecs.gdp-04.gdplogd",
+//     "gcl_name": "wnRXJzfgkHhtbtst0AWU4pFR1HPOOKvPJF7Gpqt1ewA"
+// }
+//
+// ==== END PUT GCL Response Page
+//
+// Info: created GCL wnRXJzfgkHhtbtst0AWU4pFR1HPOOKvPJF7Gpqt1ewA
+// bash-3.2$ 
+
 $(document).ready(function() {
 
     document.getElementById("method_1").innerHTML = "POST";
-    var url_post = "https://gdp-rest-01.eecs.berkeley.edu/gdp/v2/gcl/" +
-	"zeZ2RT9awTNHNYCJAVYS5tLJHsVXqb5DUVfXYR5lqr4";
+    var url_post = "https://gdp-rest-01.eecs.berkeley.edu/gdp/v1/gcl/" +
+    	"wnRXJzfgkHhtbtst0AWU4pFR1HPOOKvPJF7Gpqt1ewA";
     document.getElementById("request_1").innerHTML = url_post;
     var time1 = new Date().toISOString();
     document.getElementById("time_1").innerHTML = time1;
@@ -10,15 +28,16 @@ $(document).ready(function() {
     $.ajax({
 	
     	beforeSend : function(xhr) {
-	    xhr.setRequestHeader('Authorization' , 'Basic ZWNkZW1vOnRlcnJhc3dhcm0=');
-	},
+    	    xhr.setRequestHeader('Authorization',
+				 'Basic ZWNkZW1vOnRlcnJhc3dhcm0=');
+    	},
     	method: "POST",
     	url: url_post,
     	contentType: "application/json",
     	data: JSON.stringify({
-	    "gcl" : "zeZ2RT9awTNHNYCJAVYS5tLJHsVXqb5DUVfXYR5lqr4",
-	    "testcase" : "POST append record " + time1
-	}),
+    	    "gcl" : "wnRXJzfgkHhtbtst0AWU4pFR1HPOOKvPJF7Gpqt1ewA",
+    	    "testcase" : "POST append record " + time1
+    	}),
     	error : function(xhr, textStatus, errorThrown) {
     	    $('#responsecode_1').text(xhr.status)
     	    $('#response_1').text(textStatus);
@@ -29,15 +48,16 @@ $(document).ready(function() {
     	}
     }) .done (function() {
 
-    	var url_get_last = "https://gdp-rest-01.eecs.berkeley.edu/gdp/v2/gcl/" +
-    	    "zeZ2RT9awTNHNYCJAVYS5tLJHsVXqb5DUVfXYR5lqr4?recno=-1";
+    	var url_get_last = "https://gdp-rest-01.eecs.berkeley.edu/gdp/v1/gcl/" +
+    	    "wnRXJzfgkHhtbtst0AWU4pFR1HPOOKvPJF7Gpqt1ewA?recno=-1";
     	document.getElementById("method_2").innerHTML = "GET";
     	document.getElementById("request_2").innerHTML = url_get_last;
 
     	$.ajax({
 	    
     	    beforeSend : function(xhr) {
-    		xhr.setRequestHeader('Authorization' , 'Basic ZWNkZW1vOnRlcnJhc3dhcm0=');
+    		xhr.setRequestHeader('Authorization',
+				     'Basic ZWNkZW1vOnRlcnJhc3dhcm0=');
     	    },
     	    url: url_get_last,		    
     	    error : function(xhr, textStatus, errorThrown) {
@@ -48,6 +68,6 @@ $(document).ready(function() {
     		$('#responsecode_2').text(xhr.status)
     		$('#response_2').text(JSON.stringify(data));
     	    }
-	});
+    	});
     });
 });
