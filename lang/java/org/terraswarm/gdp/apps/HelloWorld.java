fc3eeb794e8a3ce0be8f7fa4abed3f01646084c9
Nitesh Mor <mor.nitesh@gmail.com>
2018-07-22 21:52:20 -0700

    tabs => spacest

--- lang/java/org/terraswarm/gdp/apps/HelloWorld.java
+++ lang/java/org/terraswarm/gdp/apps/HelloWorld.java
@@ -38,42 +38,42 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-	try {
-	    GDP.gdp_init();
-	    GDP.dbg_set("*=10");
+        try {
+            GDP.gdp_init();
+            GDP.dbg_set("*=10");
 
-	    String logname = "edu.berkeley.eecs.mor.20160131_182143";
-	    GDP_NAME gn = new GDP_NAME(logname);
-	    System.out.println("Creating object");
-	    GDP_GIN g = new GDP_GIN(gn, GDP_GIN.GDP_MODE.RA, new GDP_NAME(""));
+            String logname = "edu.berkeley.eecs.mor.20160131_182143";
+            GDP_NAME gn = new GDP_NAME(logname);
+            System.out.println("Creating object");
+            GDP_GIN g = new GDP_GIN(gn, GDP_GIN.GDP_MODE.RA, new GDP_NAME("gdplogd.mor"));
 
-	    /*
-	      g.subscribe(0,3);
-	      for (;;) {
-	      String data = g.get_next_data(0);
-	      if (data == null) { break; }
-	      System.out.println(data);
-	      }
+            /*
+              g.subscribe(0,3);
+              for (;;) {
+              String data = g.get_next_data(0);
+              if (data == null) { break; }
+              System.out.println(data);
+              }
 
-	    */
-	    /*
-	      g.append(new Date().toString());
-	      System.out.println(g.read(-1));
-	      g.append(new Date().toString());
-	      System.out.println(g.read(-1));
-	    */
+            */
+            /*
+              g.append(new Date().toString());
+              System.out.println(g.read(-1));
+              g.append(new Date().toString());
+              System.out.println(g.read(-1));
+            */
         
-	    g.append((new Date().toString()).getBytes());
+            g.append((new Date().toString()).getBytes());
         
-	    HashMap<String, Object> returnDatum = g.read_by_recno(-1);
-	    byte[] data = (byte[]) returnDatum.get("data");
-	    for (int i=0; i<data.length; i++) {
-		System.out.print((char) data[i]);
-	    }
-	} catch (Throwable throwable) {
-	    System.err.println(throwable);
-	    throwable.printStackTrace();
-	}
+            HashMap<String, Object> returnDatum = g.read_by_recno(-1);
+            byte[] data = (byte[]) returnDatum.get("data");
+            for (int i=0; i<data.length; i++) {
+                System.out.print((char) data[i]);
+            }
+        } catch (Throwable throwable) {
+            System.err.println(throwable);
+            throwable.printStackTrace();
+        }
 
     }
 }
