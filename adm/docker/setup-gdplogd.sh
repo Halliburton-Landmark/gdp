c019d317a7d6ce8c8d84e98afa32d52efa5f6e62
Eric Allman <eric@cs.berkeley.edu>
2018-12-03 12:08:15 -0800

    Clean up and parameterize docker entries; adds UBUNTU_VER and ALPINE_VER make symbols.

--- adm/docker/setup-gdplogd.sh
+++ adm/docker/setup-gdplogd.sh
@@ -2,6 +2,7 @@
 
 #
 #  Set up gdplogd configuration and database
+#	This is run when the image is built.
 #
 
 set -e
@@ -13,10 +14,14 @@ chown gdp:gdp $GDP_DATA_ROOT $GDP_DATA_ROOT/glogs
 chmod 750 $GDP_DATA_ROOT $GDP_DATA_ROOT/glogs
 
 # system log files inside container
+#	Arguably, these should be in the host filesystem tree, since
+#		logs are useful across restarts; also, I suspect chatty
+#		logs chew up memory.
 SYSLOGDIR=/var/log/gdp
 mkdir -p $SYSLOGDIR
 chown gdp:gdp $SYSLOGDIR
 
+# set up default parameters
 mkdir -p /etc/ep_adm_params
 cat > /etc/ep_adm_params/gdp <<- EOF
 	swarm.gdp.data.root=$GDP_DATA_ROOT
--- adm/docker/start-gdplogd.sh
+++ adm/docker/start-gdplogd.sh
@@ -2,17 +2,28 @@
 
 #
 #  Initialize a container for running gdplogd.
+#	This is run when a container is starting up.
 #
 
 set -e
 
-if [ ! -z "$GDP_ROUTER" ]; then
-	echo "swarm.gdp.routers=${GDP_ROUTER:=gdp-01.eecs.berkeley.edu; gdp-02.eecs.berkeley.edu}" \
-		>> /etc/ep_adm_params/gdp
+if [ -z "$GDP_ROUTER" ]; then
+	# assume use of Berkeley servers if nothing specified
+	# (be sure to include semicolon at the end of each line)
+	GDP_ROUTER=`cat <<- EOF
+		gdp-01.eecs.berkeley.edu;
+		gdp-02.eecs.berkeley.edu;
+EOF
+`
+	# eliminate newlines...
+	GDP_ROUTER=`echo $GDP_ROUTER`
 fi
+echo "swarm.gdp.routers=${GDP_ROUTER}" >> /etc/ep_adm_params/gdp
 
 # come up with unique values for this instance
 if [ -z "$GDPLOGD_NAME" ]; then
+	# pick a unique name --- note: changes on every instantiation, so
+	# not all that useful, but the alternative is worse.
 	GDPLOGD_NAME=`hostname --fqdn | \
 		tr '.' '\n' | \
 		tac | \
