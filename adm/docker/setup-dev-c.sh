e90b6dadf760df4e66d7b588497ffae7e2f94843
Eric Allman <eric@cs.berkeley.edu>
2019-02-28 13:42:08 -0800

    Remove "install-doc" in Docker image to avoid having to install pandoc, which pulls in lots of dependencies.  Documentation shouldn't be needed inside a container in most cases.

--- adm/docker/setup-dev-c.sh
+++ adm/docker/setup-dev-c.sh
@@ -5,11 +5,19 @@
 #	Installs the client code.
 #	Removes the source tree, except for adm, which is used by other
 #		builds.
+#	Doesn't install documentation, because that requires pandoc,
+#		which pulls in a huge number of dependencies.  This
+#		should be OK since humans tend to work outside
+#		containers rather than in them.  If it's needed,
+#		it will be necessary to run:
+#			sudo apt install pandoc
+#			cd /src/gdp
+#			sudo make install-doc
 #
 
 set -e
 
 cd /src/gdp
-make install-client install-doc
+make install-client
 srcfiles=`ls | grep -v '^adm$'`
 rm -r $srcfiles
